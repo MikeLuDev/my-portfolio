76c643a12e95441a575cf59403c865ed37e96610
Michael Lu <michaelwlu@live.com>
2018-03-01 23:51:06 -0500

    Split index.scss into separate files lined by import

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.scss'
+import '../style/main.scss'
 
 let links = ["home", "blog", "music", "portfolio", "contact"];
 

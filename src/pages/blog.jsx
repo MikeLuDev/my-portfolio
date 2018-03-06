badf78221d1b8d68b53ef7de4a0be7c28363691c
Michael Lu <michaelwlu@live.com>
2018-03-06 17:13:32 -0500

    Made first blog post and established blog post formatting

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -3,15 +3,17 @@ import Link from 'gatsby-link';
 
 const Blog = ({ data }) => (
   <div className="blog-index">
-    <h1>This is a blog, I guess.</h1>
+    <h1>Blog</h1>
     <p>{data.allMarkdownRemark.totalCount} {data.allMarkdownRemark.totalCount == 1 ? "post" : "posts"}</p>
     {data.allMarkdownRemark.edges.map(({ node }) => (
-      <Link to={node.fields.slug} >
-          <div className="blog-post-link" key={node.id}>
-            <h3>{`${node.frontmatter.title} - ${node.frontmatter.date}`}</h3>
-            <p>{node.excerpt}</p>
-          </div>
-        </Link>
+      <Link to={node.fields.slug}>
+        <div className="blog-link">
+            <div className="blog-link-head" key={node.id}>
+              <h3>{`${node.frontmatter.title}`}</h3>
+              <p>{`on ${node.frontmatter.date}`}</p>
+            </div>
+        </div>
+      </Link>
       ))}
   </div>
 );
@@ -20,7 +22,7 @@ const Blog = ({ data }) => (
 // TODO: implement sorting
 export const query = graphql `
   query IndexQuery {
-    allMarkdownRemark(sort: {fields: [frontmatter___date], order: ASC}) {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       totalCount
       edges {
         node {
---
+++ src/posts/hello-world.md
@@ -0,0 +1,22 @@
+---
+title: "Hello World"
+date: "2018-03-06"
+---
+
+## Subheading
+
+### Heading 3
+
+First post? Here we will test some text and see just what it will do.
+
+<img src="/img/projects/calc.png">
+
+And another paragraph?
+
+```
+function testCode(code) {
+    if (code is good) {
+        then do some stuff;
+    }
+}
+```

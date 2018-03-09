0636ef61836037daa96f720b17a689ed562c2c13
Michael Lu <michaelwlu@live.com>
2018-03-09 09:41:16 -0500

    Move under construction splash into new components folder

--- src/pages/music.jsx
+++ src/pages/music.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import UnderConstruction from './underconstruction.jsx';
+import UnderConstruction from '../components/underconstruction.jsx';
 
 class Music extends React.Component {
     constructor(props) {
